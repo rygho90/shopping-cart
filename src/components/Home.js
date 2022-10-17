90c0841f300af0176123e2c14416b441a790e6f8
Ryan <rygho90@gmail.com>
2022-10-17 07:12:46 -0400

    Set up basic HTML for Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <header className="header">
+        <div className="container">
+          <h1 className="header-title">RFS</h1>
+          <nav className="header-nav">
+            <ul className="nav-list">
+              <li>Home</li>
+              <li>Shop</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+          <button>Cart</button>
+        </div>
+      </header>
+
+      <main>
+        <section>
+            <div>
+                <h2>Ryan's Fitness Store</h2>
+                <p>Serving all your fitness needs since 2022.</p>
+                <p>Visit the shop</p>
+            </div>
+        </section>
+      </main>
+
+      <footer>
+        <div>&copy; 2022 Ryan Holland</div>
+      </footer>
+    </>
+  );
 }
