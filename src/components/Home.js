ae339deecde33b898915099439cfedbd7f077312
Ryan <rygho90@gmail.com>
2022-10-18 07:44:48 -0400

    Style header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,32 @@
 import React from "react";
-import "../css/home.css"
-
+import "../css/home.css";
 
 export default function Home() {
   return (
     <>
       <header className="header">
-        <div className="container">
-          <h1 className="header-title">RFS</h1>
-          <nav className="header-nav">
-            <ul className="nav-list">
-              <li>Home</li>
-              <li>Shop</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-          <button>Cart</button>
+        <div className="header-container">
+          <h1 className="header-title">Ryan's Fitness Store</h1>
+          <div className="header-right-side">
+            <nav className="header-nav">
+              <ul className="nav-list">
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+            <button className="cart-button"></button>
+          </div>
         </div>
       </header>
 
       <main>
         <section>
-            <div>
-                <h2>Ryan's Fitness Store</h2>
-                <p>Serving all your fitness needs since 2022.</p>
-                <p>Visit the shop</p>
-            </div>
+          <div>
+            <h2>Ryan's Fitness Store</h2>
+            <p>Serving all your fitness needs since 2022.</p>
+            <p>Visit the shop</p>
+          </div>
         </section>
       </main>
 
