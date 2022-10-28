b4875fc71d8b81492f41c536c1c9c6c176098d61
Ryan <rygho90@gmail.com>
2022-10-28 07:08:01 -0400

    Add basic styling for shop search bar and category filter

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -7,10 +7,24 @@ export default function Shop() {
 
   return (
     <main>
-      <div className="main-div shop-container">
-        {items.map((item) => {
-          return <ShopItem key={item.id} {...item} />;
-        })}
+      <div className="main-div search-div">
+        <input type="text" className="shop-search"></input>
+        <button className="search-button"></button>
+      </div>
+      <div className="shop-wrapper">
+        <div className="shop-side-bar main-div">
+          <ul className="nav-list category-list">
+            <li>All</li>
+            <li>Equipment</li>
+            <li>Supplements</li>
+            <li>Clothing</li>
+          </ul>
+        </div>
+        <div className="main-div shop-container">
+          {items.map((item) => {
+            return <ShopItem key={item.id} {...item} />;
+          })}
+        </div>
       </div>
     </main>
   );
